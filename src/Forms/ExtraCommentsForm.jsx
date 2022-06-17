f026a85a58d8ba22fc3744f37d4d8e1879957f91
Jean-Luc LaCosse <jeanlacosse@gmail.com>
2022-06-16 21:44:47 -0500

    forms

--- src/Forms/ExtraCommentsForm.jsx
+++ src/Forms/ExtraCommentsForm.jsx
@@ -5,10 +5,17 @@ function ExtraCommentsForm () {
 
     const dispatch = useDispatch()
 
+    // TODO
+    // create useState for form value
+    // create html form to fill out
+    // send state in payload w/ dispatch
+    // create store in index.js
+    // add action.payload to store in function
+
     return (
         dispatch({
             type: 'ADD_EXTRA_COMMENTS',
-            payload: // send over the state of thh form values
+            payload: // send over the state of the form values
         })
 
     )
