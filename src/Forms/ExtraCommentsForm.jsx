59ecdfaed1f11455385c90a793a35cd186fdbd62
Jean-Luc LaCosse <jeanlacosse@gmail.com>
2022-06-17 08:44:04 -0500

    created store

--- src/Forms/ExtraCommentsForm.jsx
+++ src/Forms/ExtraCommentsForm.jsx
@@ -1,9 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 
+
+
 function ExtraCommentsForm () {
 
     const dispatch = useDispatch()
+
+    const [extraComments, setExtraComments] = useState('')
+
+    const handleComments = (event) => {
+        event.preventDefault();
+        setExtraComments(event.target.value);
+
+        dispatch({
+            type: 'ADD_EXTRA_COMMENTS',
+            payload: extraComments
+        })
+      }
 
     // TODO
     // create useState for form value
@@ -11,13 +25,18 @@ function ExtraCommentsForm () {
     // send state in payload w/ dispatch
     // create store in index.js
     // add action.payload to store in function
+   
 
     return (
-        dispatch({
-            type: 'ADD_EXTRA_COMMENTS',
-            payload: // send over the state of the form values
-        })
+        <form onSubmit={(event) => addProduct(event)}>
+      <input
+        onChange={handleComments}
+        type='text'
+        placeholder='Comments'
+      />
 
+      <button type='submit'>Next</button>
+    </form>
     )
 }
 
